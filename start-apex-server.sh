11b0240192288a17650e3349ba430f3e76bd89f8
Dang Mai <contact@dangmai.net>
2022-06-03 23:48:56 -0400

    Add comment

--- start-apex-server.sh
+++ start-apex-server.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 nohup node node_modules/.bin/start-apex-server >/dev/null 2>&1 &
+# Wait until server is up, this is a simple static sleep but you can use
+# something like wait-on
 sleep 2
